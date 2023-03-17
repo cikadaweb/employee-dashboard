701f99d2864079e2eea916f2628eb3dfa06fa97a
cikada <adel.garipovazn@mail.ru>
2023-03-18 01:33:53 +0300

    Init project

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-app>
+    <TheHeader />
+    <v-main>
+    </v-main>
+  </v-app>
+</template>
+
+<script setup lang="ts">
+import TheHeader from '@/components/TheHeader.vue'
+</script>
