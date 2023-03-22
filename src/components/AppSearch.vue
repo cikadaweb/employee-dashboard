3245288a727e1729f8399e63dd91eac609b7ad6e
cikada <adel.garipovazn@mail.ru>
2023-03-23 00:23:54 +0300

    [WIP] AppSearch

---
+++ src/components/AppSearch.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-card class="mb-1 elevation-0">
+    <v-card-text
+      class="px-7 py-7 mb-1">
+      <v-text-field
+        bg-color="blue-grey-lighten-5"
+        density="compact"
+        variant="solo"
+        label="Поиск сотрудника"
+        append-inner-icon="mdi-magnify"
+        hint="Например: Ивано Иван"
+        persistent-hint
+        single-line
+      ></v-text-field>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
