3238b20ed96a385c25db9a1d040848e2e0479162
cikada <adel.garipovazn@mail.ru>
2023-03-23 00:27:18 +0300

    Create store

--- src/store/app.ts
+++ src/store/app.ts
@@ -1,8 +1,44 @@
 // Utilities
 import { defineStore } from 'pinia'
+import  axios  from 'axios';
+
+interface IStatus {
+  tag_id: number,
+  tag: string,
+  description: string,
+}
+interface IEmployee {
+  id: 1,
+  full_name: string,
+  inn: string,
+  address: string,
+  date_birth: string,
+  age: number,
+  type_contract: string,
+  type_contact_id: number,
+  gender: string,
+  gender_id: number,
+  country: string,
+  country_id: number,
+  position: string,
+  position_id: number,
+  status: IStatus,
+}
 
 export const useAppStore = defineStore('app', {
   state: () => ({
-    //
+    employees: [] as IEmployee[]
   }),
+  actions: {
+    async getEmployess() {
+      const response = await axios.get('https://employee-manager-8c728-default-rtdb.firebaseio.com/employees.json');
+      const data = await response.data;
+      this.employees = data;
+    }
+  },
+  getters: {
+    allEmployees() {
+      return this.employees;
+    },
+  },
 })
