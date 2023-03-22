d8853759a0d69fd78d1168262a116fe2e568a68e
cikada <adel.garipovazn@mail.ru>
2023-03-23 00:26:18 +0300

    [WIP] Create Users Dashboard

---
+++ src/components/UsersDashboard.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <AppSearch />
+    <UsersPanel />
+  </div>
+</template>
+
+<script setup lang="ts">
+import AppSearch from '@/components/AppSearch.vue'
+import UsersPanel from '@/components/UsersPanel.vue'
+</script>
+
+<style scoped>
+
+</style>
