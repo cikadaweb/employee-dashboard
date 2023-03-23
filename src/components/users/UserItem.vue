4da8786831d53213347fcf85ee9c7bc839e5d125
cikada <adel.garipovazn@mail.ru>
2023-03-23 23:18:16 +0300

    Refactor interfaces

--- src/components/users/UserItem.vue
+++ src/components/users/UserItem.vue
@@ -48,28 +48,7 @@ import {
   defineProps, PropType, computed
 } from 'vue';
 
-interface IStatus {
-  tag_id: number,
-  tag: string,
-  description: string,
-}
-interface IEmployee {
-  id: 1,
-  full_name: string,
-  inn: string,
-  address: string,
-  date_birth: string,
-  age: number,
-  type_contract: string,
-  type_contact_id: number,
-  gender: string,
-  gender_id: number,
-  country: string,
-  country_id: number,
-  position: string,
-  position_id: number,
-  status: IStatus,
-}
+import { IEmployee } from '@/store/employee.interfaces'
 
 const props = defineProps({
   item: {
